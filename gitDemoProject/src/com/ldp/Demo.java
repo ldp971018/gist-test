01f017f3f7a4df31a46db7b4735c0bad36518e50
T-liudp <1846882398@qq.com>
2022-03-14 23:57:58 +0800

    添加一个demo project

---
+++ gitDemoProject/src/com/ldp/Demo.java
@@ -0,0 +1,12 @@
+package com.ldp;
+
+/**
+ * @author T-liudp
+ * @create 2022-03-14 23:56
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+    }
+}
